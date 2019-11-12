7678d6bf0056f31ea86304118ddac3b5a1c42488
glozanoa <glozanoa@uni.pe>
2019-11-12 07:53:20 -0500

    Algoritmo depurado

--- code/rlc_circuit_menu.py
+++ code/rlc_circuit_menu.py
@@ -30,10 +30,63 @@ def runge_kutta_sist(x0, f, h, a, b):
     output:
         x = [x0, ..., xk, ... xn]: xk= aproximacion de la solucion del sistema de odes tk = a + k*h.  
     """
+    if isinstance(x0, np.ndarray):
+        t = np.arange(a, b+h/2, h, dtype=np.longdouble)
+        #x = np.array([x0])  # Solucion del sistema de odes
+        rows, cols = np.shape(x0)
+        x = np.zeros((len(t), rows, cols), dtype=np.longdouble)
+        x[0] = x0
+        tt = t[:len(t)-1]
+        for k, tk in enumerate(tt):
+            k1 = f(tk , x[k])
+            aux2 = x[k]+h*k1/2 #variable de depuracion
+            k2 = f(tk + h/2, x[k]+h*k1/2)
+            aux3 = x[k]+h*k2/2 #variable de depuracion
+            k3 = f(tk + h/2, x[k]+h*k2/2)
+            aux4 = x[k]+h*k3   #variable de depuracion
+            k4 = f(tk + h, x[k]+h*k3)
+            xx = x[k] + h*(k1+2*k2+2*k3+k4)/6 # xx = x_{k+1}
+            x[k+1] = xx
+
+        return t, x
+    else:
+        t = np.arange(a, b, h)
+        #x = np.array([x0])  # Solucion del sistema de odes
+        #rows, cols = np.shape(x0)
+        x = np.zeros(len(t), dtype=np.longdouble)
+        x[0] = x0
+
+        for k, tk in enumerate(t[:len(t)-1]):
+            k1 = f(tk , x[k])
+            aux2 = x[k]+h*k1/2
+            k2 = f(tk + h/2, x[k]+h*k1/2)
+            aux3 = x[k]+h*k2/2
+            k3 = f(tk + h/2, x[k]+h*k2/2)
+            aux4 = x[k]+h*k3
+            k4 = f(tk + h, x[k]+h*k3)
+            xx = x[k] + h*(k1+2*k2+2*k3+k4)/6 # xx = x_{k+1}
+            x[k+1] = xx
+
+        return t, x
+
+
+""" Antigua implementacion(ERROR: indexacion en el tiempo(t))
+def runge_kutta_sist(x0, f, h, a, b):
+    
+    #Resuelve el sistema de ecuaciones diferenciales
+    #x' = f(t, x), x(t) = [x1(t), x2(t), ..., xm(t)], donde xr(t): R-> R^{p}, p natural
+    #input:
+    #    x0: valor de inicio (manteniendo la estructura de x(t))
+    #    f: funcion vectorial (camino)
+    #    a, b: cota inferior y superior (resp) del intervalo [a, b]
+    #    h: tamaño de paso
+    #output:
+    #    x = [x0, ..., xk, ... xn]: xk= aproximacion de la solucion del sistema de odes tk = a + k*h.  
+    
 
     t = np.arange(a, b, h)
     #x = np.array([x0])  # Solucion del sistema de odes
-    x = np.ndarray((len(t), 2, 3))
+    x = np.ndarray((len(t), 2, 3), dtype=np.longdouble)
     x[0] = x0
 
     for k, tk in enumerate(t[:len(t)-1]):
@@ -48,7 +101,7 @@ def runge_kutta_sist(x0, f, h, a, b):
         x[k+1] = xx
 
     return t, x
-
+"""
 def genera_funcion_sist(pulso, amp, frec, induc, resist, capacit):
     if pulso == "sinusoidal":
         pulso_volt = lambda t: amp*np.sin(2*pi*frec*t)
@@ -60,8 +113,8 @@ def genera_funcion_sist(pulso, amp, frec, induc, resist, capacit):
         pulso_volt = lambda t: amp*sg.sawtooth(2*np.pi*frec*t)
     
     def f(t, x, L = induc, C = capacit, R = resist, volt = pulso_volt):
-        z = x[0, :]
-        q = x[1, :]
+        z = x[0]
+        q = x[1]
 
         # definiendo g(t, z, q) = [g1, g2, g3]
 
@@ -70,8 +123,8 @@ def genera_funcion_sist(pulso, amp, frec, induc, resist, capacit):
         #NOTA: qk = q[k-1]
 
 
-        g1 = lambda t, z, q: V(t)/L[0] - ((L[0]+L[1])/(C[0]*L[0]*L[1]))*q[0] + 1/(L[1]*C[1])*q[1] - R[0]/L[0]*z[0] - (L[1]/R[1]-L[0]/R[0])*(z[1]+z[2])
-        g2 = lambda t, z, q: q[0]/(L[1]*C[0]) - (L[1]+L[2])/(C[1]*L[1]*L[2])*q[1] + 1/(C[2]*L[2])*q[2] - (R[1]/L[1])*(z[1]+z[2])
+        g1 = lambda t, z, q: V(t)/L[0] - ((L[0]+L[1])/(C[0]*L[0]*L[1]))*q[0] + 1/(L[1]*C[1])*q[1] - (R[0]/L[0])*z[0] - (L[1]/R[1]-L[0]/R[0])*(z[1]+z[2])
+        g2 = lambda t, z, q: q[0]/(L[1]*C[0]) - (L[1]+L[2])/(C[1]*L[1]*L[2])*q[1] + q[2]/(C[2]*L[2]) - (R[1]/L[1])*(z[1]+z[2])
         g3 = lambda t, z, q: q[1]/(L[2]*C[1]) - q[2]/(C[2]*L[2]) - (R[2]/L[2])*z[2] 
 
         g = lambda t, z, q: np.array([g1(t, z, q), g2(t, z, q), g3(t, z, q)])
@@ -79,9 +132,9 @@ def genera_funcion_sist(pulso, amp, frec, induc, resist, capacit):
         f1 = lambda t, z, q: g(t, z, q)
         f2 = lambda t, z, q: z
 
-        f = lambda t, z, q: np.array([f1(t, z, q), f2(t, z, q)])
+        F = lambda t, z, q: np.array([f1(t, z, q), f2(t, z, q)], dtype=np.longdouble)
 
-        return f(t, z, q)
+        return F(t, z, q)
     
     return f
 
@@ -169,9 +222,9 @@ def plot_volt_condesador(t, q, C):
     v2 = q2/C[2]
 
 
-    plt.plot(t, v0, label="q0(t)")
-    plt.plot(t, v1, label="q1(t)")
-    plt.plot(t, v2, label="q2(t)")
+    plt.plot(t, v0, label="V0(t) en C0")
+    plt.plot(t, v1, label="V1(t) en C1")
+    plt.plot(t, v2, label="V2(t) en C2")
     plt.title("Voltaje capacitores (C0, C1, C2)")
     plt.legend()
     plt.grid()
@@ -182,10 +235,14 @@ def plot_volt_resist(t, i, R):
     i0 = np.array([ik[0] for ik in i])
     i1 = np.array([ik[1] for ik in i])
     i2 = np.array([ik[2] for ik in i])
-    
-    v0 = i0*R[0]
-    v1 = i1*R[1]
-    v2 = i2*R[2]
+
+    i_r1 = i0 + i1+ i2
+    i_r2 = i1 +i2
+    i_r3 = i2
+
+    v0 = i_r1*R[0]
+    v1 = i_r2*R[1]
+    v2 = i_r3*R[2]
 
 
     plt.plot(t, v0, label="V0(t)")
@@ -209,7 +266,7 @@ def plot_charge_condensador(t, q):
     plt.legend()
     plt.grid()
     plt.show()
-######
+
     
 
 
@@ -255,16 +312,16 @@ def menu_init():
     
     if pulso == lether_option_pulso[0]:
         pulso = "sinusoidal"
-        amp = float(input("Amplitud de la onda {}: ".format(pulso)))
-        frec = float(input("Frecuencia de la onda {}: ".format(pulso)))
+        amp = float(input("Amplitud de la onda {}(vpp): ".format(pulso)))
+        frec = float(input("Frecuencia de la onda {}(hz): ".format(pulso)))
     elif pulso == lether_option_pulso[1]:
         pulso = "cuadrada"
-        amp = float(input("Amplitud de la onda {}: ".format(pulso)))
-        frec = float(input("Frecuencia de la onda {}: ".format(pulso)))
+        amp = float(input("Amplitud de la onda {}(vpp): ".format(pulso)))
+        frec = float(input("Frecuencia de la onda {}(hz): ".format(pulso)))
     elif pulso == lether_option_pulso[2]:
         pulso = "cierra"
-        amp = float(input("Amplitud de la onda {}: ".format(pulso)))
-        frec = float(input("Frecuencia de la onda {}: ".format(pulso)))
+        amp = float(input("Amplitud de la onda {}(vpp): ".format(pulso)))
+        frec = float(input("Frecuencia de la onda {}(hz): ".format(pulso)))
     #elif pulso == lether_option_pulso[-1]:
     #    sys.exit(1)
     """
@@ -279,7 +336,7 @@ def abecedario(n):  # genera las n primero letras del abecedario
         return [chr(x) for x in range(97, 97+n)]
     else:
         n = 26
-        print("n = {}".format(n))
+        #print("n = {}".format(n))
         return abecedario(n)
 
 
@@ -300,16 +357,16 @@ def options_menu(options):
 
 
 def menu_operation(t, i, q, L , R, C):
-    options_graph = ["Graficar corriente vs tiempo", "Graficar Voltaje vs tiempo", "Graficar carga vs tiempo", "Inicializar denuevo(ERROR NO FUNCIONA)","Exit"]
+    options_graph = ["Graficar corriente vs tiempo", "Graficar Voltaje vs tiempo", "Graficar carga vs tiempo", "Inicializar denuevo","Exit"]
     graph , lether_option_graph = options_menu(options_graph)
 
 
-    if graph == lether_option_graph[0]:
+    if graph == lether_option_graph[0]: # Graficas de corriente
         options_current = ["Corriente en inductores", "Corriente en condensadores" , \
                         "Corriente en resistencias", "Volver al menú de graficos"] 
         current, lether_option_current = options_menu(options_current)
         
-        if current == lether_option_current[0]: # Graficas de corriente
+        if current == lether_option_current[0]: 
             plot_current_inductor(t, i)
             menu_operation(t, i, q, L , R, C)
 
@@ -327,7 +384,6 @@ def menu_operation(t, i, q, L , R, C):
     elif graph == lether_option_graph[1]: # Graficas de voltaje
         options_volt = ["Voltaje en inductores(FALTA IMPLEMENTAR)", "Voltaje en condensadores" , \
                         "Voltaje en resistencias", "Volver al menú de graficos"] 
-        #current = options_menu_graph(options_volt)
         volt, lether_option_volt = options_menu(options_volt)
         
         if volt == lether_option_volt[0]:
@@ -347,7 +403,7 @@ def menu_operation(t, i, q, L , R, C):
 
 
 
-    elif graph == lether_option_graph[2]:
+    elif graph == lether_option_graph[2]:   # Graficas de cargas
         options_charge = ["Carga en condensadores", "Volver al menú de graficos"] 
         
         charge, lether_option_charge = options_menu(options_charge)
@@ -359,13 +415,11 @@ def menu_operation(t, i, q, L , R, C):
         elif charge == lether_option_charge[-1]:
             menu_operation(t, i, q, L , R, C)
 
-    elif graph == lether_option_current[3]:
-        #pass
+    elif graph == lether_option_graph[3]:   # Volver a menu inicial
         simulacion_rlc()
 
-    elif lether_option_graph[-1]:
+    elif graph == lether_option_graph[-1]:  # terminar programa
         sys.exit(1)
-        #menu_operation(t, i, q, L , R, C)
         
 
 
@@ -377,7 +431,21 @@ def menu_operation(t, i, q, L , R, C):
 
 def inicializacion():
     #NOTA: x0 = [z0, q0] = [q'(0), q(0)] = [i(0), q(0)], donde i = (i1, i2, i3) y q = (q1, q2, q3)
-    x0 = np.array([[0, 0, 0], [0, 0, 0]], dtype=np.double)
+    x0 = np.array([[0, 0, 0], [0, 0, 0]], dtype=np.longdouble)
+    """
+    L0 = 18.31e-3    # Asignar valor real del inductancia
+    L1 = 11.27e-3    # Asignar valor real del inductancia
+    L2 = 17.51e-3    # Asignar valor real del inductancia
+
+    C0 = 218e-6    # Asignar valor real de la capacitancia
+    C1 = 481e-6    # Asignar valor real de la capacitancia
+    C2 = 997e-6    # Asignar valor real de la capacitancia
+
+
+    R0 = 4.355e3    # Asignar valor real de la resistencia
+    R1 = 3.334e3    # Asignar valor real de la resistencia
+    R2 = 5.269e3    # Asignar valor real de la resistencia
+    """
 
     L0 = 1.0    # Asignar valor real del inductancia
     L1 = 1.0    # Asignar valor real del inductancia
@@ -392,10 +460,9 @@ def inicializacion():
     R1 = 1.0    # Asignar valor real de la resistencia
     R2 = 1.0    # Asignar valor real de la resistencia
 
-
-    L = [L0, L1, L2]
-    C = [C0, C1, C2]
-    R = [R0, R1, R2]
+    L = np.array([L0, L1, L2], dtype=np.longdouble)
+    C = np.array([C0, C1, C2], dtype=np.longdouble)
+    R = np.array([R0, R1, R2], dtype=np.longdouble)
 
 
     a, b, h, pulso, amp, frec = menu_init()
@@ -403,7 +470,7 @@ def inicializacion():
     f = genera_funcion_sist(pulso, amp, frec, L, R, C)    
         
     # resolviendo el sistema de odes mediante rk4, x = [i, q]
-
+    print("Procesando Datos ...")
     t, x = runge_kutta_sist(x0, f, h, a, b)
 
     i = np.array([xk[0] for xk in x])   # i = (i0, i1, i2)
